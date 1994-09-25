ac1fa7db11ac9c1f919eef1177b4defecac1d9bd
Henrik Frystyk Nielsen <frytstik@w3.org>
1994-09-25 13:12:35 +0000

    minor change in GridStyle.c

--- LineMode/src/GridStyle.c
+++ LineMode/src/GridStyle.c
@@ -28,6 +28,21 @@ typedef struct {
 } HTTabStop;
 
 
+typedef long int HTLMFont;	/* For now */
+
+#define HT_NON_BREAK_SPACE ((char)1)	/* For now */
+
+#define HT_FONT		0
+#define HT_CAPITALS	1
+#define HT_BOLD		2
+#define HT_UNDERLINE	4
+#define HT_INVERSE	8
+#define HT_DOUBLE	0x10
+
+#define HT_BLACK	0
+#define HT_WHITE	1
+
+
 struct _HTStyle {
 
 /*      Style management information
