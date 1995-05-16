87466113f3028ba5bf38f82cb7192fbb5cc87f89
Charlie Brooks <cbrooks@osf.org>
1995-05-16 20:17:56 +0000

    OSF/RI NT Changes

---
+++ Library/src/HTEvnttd.c
@@ -0,0 +1,32 @@
+/* Implementation dependent include files */
+#include "tcp.h"
+
+/* Library include files */
+#include "HTUtils.h"
+#include "HTAccess.h"
+#include "HTError.h"
+#include "HTThread.h"
+#include "HTList.h"
+
+#include <HTEvent.h>
+#include <HTThread.h> 
+
+PUBLIC void HTEventThread_new ARGS1(void *, new_net) 
+{
+    return HTThread_new( (HTNetInfo *)new_net ) ;
+}
+
+PUBLIC int HTEventThread_clear ARGS1( void *, old_net) 
+{
+    return HTThread_clear( (HTNetInfo *)old_net ) ;
+}
+
+PUBLIC void HTEventThreadState ARGS2( SOCKFD, sockfd, HTThreadAction, action) 
+{
+    HTThreadState( sockfd, action ) ;
+}    
+
+    
+    
+
+    
