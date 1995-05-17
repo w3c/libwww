1d6eee5f0be5aacc91480c10354f12abd59f1292
Henrik Frystyk Nielsen <frytstik@w3.org>
1995-05-17 23:46:53 +0000

    void function can't return a function

--- Library/src/HTEvnttd.c
+++ Library/src/HTEvnttd.c
@@ -7,18 +7,17 @@
 #include "HTError.h"
 #include "HTThread.h"
 #include "HTList.h"
-
-#include <HTEvent.h>
-#include <HTThread.h> 
+#include "HTEvent.h"
+#include "HTThread.h" 
 
 PUBLIC void HTEventThread_new ARGS1(void *, new_net) 
 {
-    return HTThread_new( (HTNetInfo *)new_net ) ;
+    HTThread_new((HTNetInfo *) new_net);
 }
 
 PUBLIC int HTEventThread_clear ARGS1( void *, old_net) 
 {
-    return HTThread_clear( (HTNetInfo *)old_net ) ;
+    HTThread_clear((HTNetInfo *) old_net);
 }
 
 PUBLIC void HTEventThreadState ARGS2( SOCKFD, sockfd, HTThreadAction, action) 
