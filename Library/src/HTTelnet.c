e00158108ddc5bdeeb213def426422c0469eb2e0
Henrik Frystyk Nielsen <frytstik@w3.org>
1995-08-08 22:58:37 +0000

    new HTTelnet module

--- Library/src/HTTelnet.c
+++ Library/src/HTTelnet.c
@@ -8,6 +8,7 @@
 **	TBL	Tim Berners-Lee timbl@w3.org
 **	JFG	Jean-Francois Groff jgh@next.com
 **	DD	Denis DeLaRoca (310) 825-4580  <CSP1DWD@mvs.oac.ucla.edu>
+**	HFN	Henrik Frystyk
 ** History
 **       8 Jun 92 Telnet hopping prohibited as telnet is not secure (TBL)
 **	26 Jun 92 When over DECnet, suppressed FTP, Gopher and News. (JFG)
@@ -23,6 +24,7 @@
 #include "HTUtils.h"
 #include "HTParse.h"
 #include "HTAnchor.h"
+#include "HTChunk.h"
 #include "HTAccess.h"
 #include "HTAlert.h"
 #include "HTTelnet.h"					 /* Implemented here */
@@ -38,241 +40,233 @@
 */
 PRIVATE void make_system_secure ARGS1(char *, str)
 {
-	char *ptr1, *ptr2;
+    char *ptr1, *ptr2;
+    if ((str == NULL)||(*str == '\0'))
+	return;
 
-	if ((str == NULL)||(*str == '\0'))
-	{
-		return;
+    /*
+     * remove leading '-' or '+' by making it into whitespace that
+     * will be stripped later.
+     */
+    if (*str=='-' || *str=='+')
+	*str = ' ';
+    ptr1 = ptr2 = str;
+    while (*ptr1) {
+	if ((!isalpha((int)*ptr1))&&(!isdigit((int)*ptr1))&&
+	    (*ptr1 != '.')&&(*ptr1 != '_')&&
+	    (*ptr1 != '+')&&(*ptr1 != '-')) {
+	    ptr1++;
+	} else {
+	    *ptr2 = *ptr1;
+	    ptr2++;
+	    ptr1++;
 	}
-
-	/*
-	 * remove leading '-' or '+' by making it into whitespace that
-	 * will be stripped later.
-	 */
-	if ((*str == '-')||(*str == '+'))
-	{
-		*str = ' ';
-	}
-
-	ptr1 = ptr2 = str;
-
-	while (*ptr1 != '\0')
-	{
-		if ((!isalpha((int)*ptr1))&&(!isdigit((int)*ptr1))&&
-			(*ptr1 != '.')&&(*ptr1 != '_')&&
-			(*ptr1 != '+')&&(*ptr1 != '-'))
-		{
-			ptr1++;
-		}
-		else
-		{
-			*ptr2 = *ptr1;
-			ptr2++;
-			ptr1++;
-		}
-	}
-	*ptr2 = *ptr1;
+    }
+    *ptr2 = *ptr1;
 }
-
-
 
 /*	Telnet or "rlogin" access
 **	-------------------------
 */
-PRIVATE int remote_session ARGS2(char *, access, char *, host)
+PRIVATE int remote_session ARGS1(char *, url)
 {
-	char * user = host;
-	char * hostname = strchr(host, '@');
-	char * port = strchr(host, ':');
-	char   command[256];
-	enum _login_protocol { telnet, rlogin, tn3270 } login_protocol =
-		strcmp(access, "rlogin") == 0 ? rlogin :
-		strcmp(access, "tn3270") == 0 ? tn3270 : telnet;
-	
+    int status = HT_NO_DATA;
+    HTChunk *cmd = HTChunkCreate(64);
+    char *access = HTParse(url, "", PARSE_ACCESS);
+    char *host = HTParse(url, "", PARSE_HOST);
+    char *hostname = strchr(host, '@');
+    char *user = NULL;
+    char *passwd = NULL;
+    char *port = NULL;
+
+    /* Look for user name, password, and port number */
+    if (hostname) {
+	*hostname++ = '\0';
+	user = host;
+	if ((passwd = strchr(host, ':')) != NULL) {
+	    *passwd++ = '\0';
+	    HTUnEscape(passwd);
+	}
+	HTUnEscape(user);			/* Might have a funny userid */
+    } else {
+	hostname = host;
+    }
+    if ((port = strchr(hostname, ':')) != NULL)
+	*port++ = '\0';
+
+    /* If the person is already telnetting etc, forbid hopping */
+    if (HTClientHost) {
+	HTChunk *msg = HTChunkCreate(256);
+	HTChunkPuts(msg, "Sorry, but the service you have selected is one ");
+	HTChunkPuts(msg, "to which you have to log in. If you were running ");
+	HTChunkPuts(msg, "locally on your own computer, you would be ");
+	HTChunkPuts(msg, "connected automatically. For security reasons, ");
+	HTChunkPuts(msg, "this is not allowed when you log in to this ");
+	HTChunkPuts(msg, "information service remotely. ");
 	if (hostname) {
-	    *hostname++ = 0;	/* Split */
-	} else {
-	    hostname = host;
-	    user = 0;		/* No user specified */
+	    HTChunkPuts(msg,"You can manually connect to this service using ");
+	    HTChunkPuts(msg, access);
+	    HTChunkPuts(msg, " to host ");
+	    HTChunkPuts(msg, hostname);
+	    if (port) {
+		HTChunkPuts(msg, " using port ");
+		HTChunkPuts(msg, port);
+	    }
+	    if (user) {
+		HTChunkPuts(msg, " and user name ");
+		HTChunkPuts(msg, user);
+	    }
+	    if (passwd) {
+		HTChunkPuts(msg, " and passwd ");
+		HTChunkPuts(msg, passwd);
+	    }
 	}
-	if (port) *port++ = 0;	/* Split */
+	HTAlert(msg->data);
+	HTChunkFree(msg);
+	free(access);
+	free(host);
+	return HT_NO_DATA;
+    }
 
-	/*
-	 * Make user and hostname secure by removing leading '-' or '+'.
-	 * and allowing only alphanumeric, '.', '_', '+', and '-'.
-	 */
-	make_system_secure(user);
-	make_system_secure(hostname);
+    /*
+    ** Make user and hostname secure by removing leading '-' or '+'.
+    ** and allowing only alphanumeric, '.', '_', '+', and '-'.
+    */
+    make_system_secure(user);
+    make_system_secure(passwd);
+    make_system_secure(hostname);
+    make_system_secure(port);
 
-/* If the person is already telnetting etc, forbid hopping */
-/* This is a security precaution, for us and remote site */
-
-	if (HTSecure) {
-	    printf("\n\nSorry, but the service you have selected is one\n");
-	    printf("to which you have to log in.  If you were running www\n");
-	    printf("on your own computer, you would be automatically connected.\n");
-	    printf("For security reasons, this is not allowed when\n");
-	    printf("you log in to this information service remotely.\n\n");
-
-	    printf("You can manually connect to this service using %s\n",
-		   access);
-	    printf("to host %s", hostname);
-	    if (user) printf(", user name %s", user);
-	    if (port) printf(", port %s", port);
-	    printf(".\n\n");
-	    return HT_NO_DATA;
-	}
-
-/* Not all telnet servers get it even if user name is specified
-** so we always tell the guy what to log in as
-*/
-        if (user) printf("When you are connected, log in as %s\n", user);
-	
-#ifdef NeXT
-#define TELNET_MINUS_L
-#endif
-#ifdef ultrix
-#define TELNET_MINUS_L
-#endif
-
-#ifdef TELNET_MINUS_L
-	sprintf(command, "%s%s%s %s %s", access,
-		user ? " -l " : "",
-		user ? user : "",
-		hostname,
-		port ? port : "");
-
-	if (TRACE) fprintf(TDEST, "HTaccess: Command is: %s\n", command);
-	system(command);
-	return HT_NO_DATA;		/* Ok - it was done but no data */
-#define TELNET_DONE
-#endif
-
-/* Most unix machines suppport username only with rlogin */
-#ifdef unix
-#ifndef TELNET_DONE
-	if (login_protocol != rlogin) {
-	    sprintf(command, "%s %s %s", access,
-		hostname,
-		port ? port : "");
-	} else {
-	    sprintf(command, "%s%s%s %s %s", access,
-		user ? " -l " : "",
-		user ? user : "",
-		hostname,
-		port ? port : "");
-	}
-	if (TRACE) fprintf(TDEST, "HTaccess: Command is: %s\n", command);
-	system(command);
-	return HT_NO_DATA;		/* Ok - it was done but no data */
-#define TELNET_DONE
-#endif
-#endif
-
-#ifdef MULTINET				/* VMS varieties */
-	if (login_protocol == telnet) {
-	    sprintf(command, "TELNET %s%s %s",
-		port ? "/PORT=" : "",
-		port ? port : "",
-		hostname);
-	} else if (login_protocol == tn3270) {
-	    sprintf(command, "TELNET/TN3270 %s%s %s",
-		port ? "/PORT=" : "",
-		port ? port : "",
-		hostname);
-	} else {
-	    sprintf(command, "RLOGIN%s%s%s%s %s",  /*lm 930713 */
-		user ? "/USERNAME=" : "",
-		user ? user : "",
-		port ? "/PORT=" : "",
-		port ? port : "",
-		hostname);
-	}
-	if (TRACE) fprintf(TDEST, "HTaccess: Command is: %s\n", command);
-	system(command);
-	return HT_NO_DATA;		/* Ok - it was done but no data */
-#define TELNET_DONE
-#endif
-
-#ifdef UCX
-#define SIMPLE_TELNET
-#endif
-#ifdef VM
-#define SIMPLE_TELNET
-#endif
+    if (!strcmp(access, "telnet")) {
 #ifdef SIMPLE_TELNET
-	if (login_protocol == telnet) {			/* telnet only */
-	    sprintf(command, "TELNET  %s",	/* @@ Bug: port ignored */
-		hostname);
-	    if (TRACE) fprintf(TDEST, "HTaccess: Command is: %s\n", command);
-	    system(command);
-	    return HT_NO_DATA;		/* Ok - it was done but no data */
+	HTChunkPuts(cmd, "TELNET ");
+	HTChunkPuts(cmd, hostname);			  /* Port is ignored */
+#else
+#ifdef MULTINET
+	HTChunkPuts(cmd, "TELNET ");
+	if (port) {
+	    HTChunkPuts(cmd, "/PORT=");
+	    HTChunkPuts(cmd, port);
+	    HTChunkPutc(cmd, ' ');
 	}
-#endif
+	HTChunkPuts(cmd, hostname);
+#else
+	HTChunkPuts(cmd, "telnet ");
+	HTChunkPuts(cmd, hostname);
+	if (port) {
+	    HTChunkPutc(cmd, ' ');
+	    HTChunkPuts(cmd,  port);
+	}
+#endif /* MULTINET */
+#endif /* SIMPLE_TELNET */
 
-#ifndef TELNET_DONE
-	fprintf(TDEST,
-	"Sorry, this browser was compiled without the %s access option.\n",
-		access);
-	fprintf(TDEST,
-	"\nTo access the information you must %s to %s", access, hostname);
-	if (port) fprintf(TDEST," (port %s)", port);
-	if (user) fprintf(TDEST," logging in with username %s", user);
-	fprintf(TDEST, ".\n");
-	return -1;
-#endif
+    } else if (!strcmp(access, "rlogin")) {
+#ifdef MULTINET
+	HTChunkPuts(cmd, "RLOGIN ");
+	if (user) {
+	    HTChunkPuts(cmd, "/USERNAME=");
+	    HTChunkPuts(cmd, user);
+	    HTChunkPutc(cmd, ' ');
+	}
+	if (port) {
+	    HTChunkPuts(cmd, "/PORT=");
+	    HTChunkPuts(cmd, port);
+	    HTChunkPutc(cmd, ' ');
+	}
+	HTChunkPuts(cmd, hostname);
+#else
+#ifdef RLOGIN_USER			       /* format: "hostname -l user" */
+	HTChunkPuts(cmd, "rlogin ");
+	HTChunkPuts(cmd, hostname);
+	if (user) {
+	    HTChunkPuts(cmd, "-l ");
+	    HTChunkPuts(cmd, user);
+	    HTChunkPutc(cmd, ' ');
+	}
+#else					       /* format: "-l user hostname" */
+	HTChunkPuts(cmd, "rlogin ");
+	if (user) {
+	    HTChunkPuts(cmd, "-l ");
+	    HTChunkPuts(cmd, user);
+	    HTChunkPutc(cmd, ' ');
+	}
+	HTChunkPuts(cmd, hostname);
+#endif /* RLOGIN_AFTER */
+#endif /* MULTINET */
+
+    } else if (!strcmp(access, "tn3270")) {
+#ifdef MULTINET
+	HTChunkPuts(cmd, "TELNET/TN3270 ");
+	if (port) {
+	    HTChunkPuts(cmd, "/PORT=");
+	    HTChunkPuts(cmd, port);
+	    HTChunkPutc(cmd, ' ');
+	}
+	HTChunkPuts(cmd, hostname);
+#else
+	HTChunkPuts(cmd, "tn3270 ");
+	HTChunkPuts(cmd, hostname);			  /* Port is ignored */
+#endif /* MULTINET */
+
+    } else {
+	if (PROT_TRACE)
+	    fprintf(TDEST, "Telnet...... Unknown access method: `%s\'\n",
+		    access);
+	status = HT_ERROR;
+    }
+
+    /* Now we are ready to execute the command */
+    if (PROT_TRACE)
+	fprintf(TDEST, "Telnet...... Command is `%s\'\n", cmd->data);
+    if (user) {
+	HTChunk *msg = HTChunkCreate(128);
+	HTChunkPuts(msg, "When you are connected, log in");
+	if (strcmp(access, "rlogin")) {
+	    HTChunkPuts(msg, "as <");
+	    HTChunkPuts(msg, user);
+	    HTChunkPutc(msg, '>');
+	}
+	if (passwd) {
+	    HTChunkPuts(msg, " using password <");
+	    HTChunkPuts(msg, passwd);
+	    HTChunkPutc(msg, '>');
+	}
+	HTAlert(msg->data);
+	HTChunkFree(msg);
+    }
+    system(cmd->data);
+    free(access);
+    free(host);
+    HTChunkFree(cmd);
+    return status;
+
 }
 
 /*	"Load a document" -- establishes a session
-**	------------------------------------------
+**	==========================================
 **
 ** On entry,
-**	addr		must point to the fully qualified hypertext reference.
-**
+**      request		This is the request structure
 ** On exit,
-**	returns		<0	Error has occured.
-**			>=0	Value of file descriptor or socket to be used
-**				 to read data.
-**	*pFormat	Set to the format of the file, if known.
-**			(See WWW.h)
-**
+**	returns		HT_ERROR	Error has occured or interrupted
+**			HT_NO_DATA	if return status 204 No Response
 */
 PRIVATE int HTLoadTelnet ARGS1(HTRequest *, request)
 {
-    char * access;
-    CONST char * addr = HTAnchor_physical(request->anchor);
-    char * host;
-    int status;
+    char *url;
+    if (!request || !request->anchor) {
+        if (PROT_TRACE) fprintf(TDEST, "HTLoadTelnet Bad argument\n");
+        return HT_ERROR;
+    }
 
-    if (request->output_stream) {
+    /* We must be in interactive mode */
+    if (!HTInteractive) {
         HTAlert("Can't output a live session -- it has to be interactive");
-	return HT_NO_ACCESS;
+	return HT_ERROR;
     }
-    access =  HTParse(addr, "file:", PARSE_ACCESS);
-    
-    host = HTParse(addr, "", PARSE_HOST);
-
-    /*
-     * Fix a security hole with telnet: URLs that contain semicolon
-     * after the machine name.  This would cause system commands to
-     * be executed after the telnet session is finished, e.g.
-     *
-     *		telnet://some.host; rm some_important_file
-     *
-     * or even more destructive:
-     *
-     *		telnet://some.host; rm -fr *	(don't try this!!)
-     */
-    {
-	char * semi = strchr(host,';');
-	if (semi) *semi = 0;
-    }
-
-    status = remote_session(access, host);
-
-    free(host);	
-    free(access);
-    return status;
+    url = HTAnchor_physical(request->anchor);
+    HTCleanTelnetString(url);
+    return remote_session(url);
 }
 
 
