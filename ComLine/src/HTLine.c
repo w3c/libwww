3ecdc18e69d7fd91780b6646bf4f00cf8d1b0a47
Henrik Frystyk Nielsen <frytstik@w3.org>
1995-12-01 22:21:33 +0000

    small change

--- ComLine/src/HTLine.c
+++ ComLine/src/HTLine.c
@@ -462,7 +462,6 @@ int main (int argc, char ** argv)
     /* Register our User Prompts etc in the Alert Manager */
     if (HTAlert_interactive()) {
 	HTAlert_add(HTError_print, HT_A_MESSAGE);
-	HTAlert_add(HTPrompt, HT_A_PROMPT);
 	HTAlert_add(HTPromptUsernameAndPassword, HT_A_USER_PW);
     }
 
