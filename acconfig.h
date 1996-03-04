da3a4b133d6a07763a971b88693aba70a0c388b0
Henrik Frystyk Nielsen <frytstik@w3.org>
1996-03-04 03:24:35 +0000

    new top makefiles

---
+++ acconfig.h
@@ -0,0 +1,91 @@
+/* Define to enable direct WAIS access. */
+#undef HT_DIRECT_WAIS
+
+/* Define to enable SOCKS firewall-breaching code. */
+#undef SOCKS
+
+/* Define to build Cyrillic version. */
+#undef CYRILLIC
+
+/* Define to be the version. */
+#undef W3C_VERSION
+
+/* Define this to be the prefix for cache files. */
+#undef CACHE_FILE_PREFIX
+
+/* Define this if we can select on stdin. */
+#undef TTY_IS_SELECTABLE
+
+/* Define this if the platform uses EBCDIC instead of ASCII. */
+#undef NOT_ASCII
+
+/* Define this if a typedef'd function definition may not be preceeded by 
+its ``extern'' declaration. */
+#undef NO_EXTERN_TYPEDEF_FUNC
+
+/* Define this if the compiler doesn't like pointers to undefined structs. */
+#undef NO_PTR_UNDEF_STRUCT
+
+/* Define this if it's not typedef'd automatically. */
+#undef BOOLEAN
+
+/* Define this to be the location of the resolver configuration file. */
+#undef RESOLV_CONF
+
+/* Define this to be the rlogin program name. */
+#undef RLOGIN_PROGRAM
+
+/* Define this to be the telnet program name. */
+#undef TELNET_PROGRAM
+
+/* Define this to be the tn3270 program name. */
+#undef TN3270_PROGRAM
+
+/* Define this if your telnet accepts the -l <username> option */
+#undef TELNET_MINUS_L
+
+/* Define this if it isn't in the header files.  */
+#undef u_char
+
+/* For removing a file */
+#undef unlink
+
+/* Define this if it isn't in the header files.  */
+#undef u_short
+
+/* Define this if it isn't in the header files.  */
+#undef u_long
+
+@BOTTOM@
+
+/* Define this if you have the external variable 'socket_errno'. */
+#undef HAVE_SOCKET_ERRNO
+
+/* Define this if you have the external variable 'sys_errlist'. */
+#undef HAVE_SYS_ERRLIST
+
+/* Define this if you have the external variable 'sys_nerr'. */
+#undef HAVE_SYS_NERR
+
+/* Define this if you have the uxc$inetdef.h header file. */
+#undef HAVE_UXC_INETDEF_H
+
+/* Define this if directory entries have inodes. */
+#undef HAVE_DIRENT_INO
+
+/* Define this if tm structure includes a tm_gmtoff entry. */
+#undef HAVE_TM_GMTOFF
+
+/* Define this if your ioctl understands the winsize structure. */
+#undef HAVE_WINSIZE
+
+/* Define this if the extern timezone is negative. */
+#undef NEGATIVE_TIMEZONE
+
+/* Define this if sys_errlist must be declared (if it exists). */
+#undef NEED_SYS_ERRLIST_DECLARED
+
+/* Define this if sys_nerr must be declared (if it exists). */
+#undef NEED_SYS_NERR_DECLARED
+
+
