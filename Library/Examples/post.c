0a2c97b5f2662a1b851b101b484ee3b1d945d740
Henrik Frystyk Nielsen <frytstik@w3.org>
1998-09-30 21:47:10 +0000

    Added a POST from memory sample app

--- Library/Examples/post.c
+++ Library/Examples/post.c
@@ -13,7 +13,7 @@
 **	PURPOSE. See W3C License http://www.w3.org/Consortium/Legal/ for more
 **	details.
 **
-**	Sample showing how to PUT a document to an HTTP server
+**	Sample showing how to POST data to an HTTP server
 */
 
 #include "WWWLib.h"
@@ -45,51 +45,65 @@ PRIVATE int terminate_handler (HTRequest * request, HTResponse * response,
 int main (int argc, char ** argv)
 {
     HTRequest * request = NULL;
-    HTAnchor * src = NULL;
+    HTParentAnchor * src = NULL;
     HTAnchor * dst = NULL;
-    char * src_str = NULL;
     char * dst_str = NULL;
+    char * data = NULL;
     BOOL status = NO;
 
     /* Create a new premptive client */
-    HTProfile_newNoCacheClient("libwww-PUT", "1.0");
+    HTProfile_newNoCacheClient("libwww-POST", "1.0");
 
     /* Add our own filter to update the history list */
     HTNet_addAfter(terminate_handler, NULL, NULL, HT_ALL, HT_FILTER_LAST);
 
     /* Handle command line args */
     if (argc >= 3) {
-	src_str = argv[1];
-	dst_str = argv[2];
+	dst_str = argv[1];
+	data = argv[2];
     } else {
-	printf("Type the URI of the source and the URI of the destination.\n");
-	printf("\t%s <src> <dst>\n", argv[0]);
-	printf("For example, %s http://www.w3.org http://myserver/destination.html\n",
+	printf("Type the URI of the destination you want to POST to and the contents that you want to post.\n");
+	printf("\t%s <destination> <data>\n", argv[0]);
+	printf("For example, %s http://myserver/destination.html \"This is some testdata\"\n",
 	       argv[0]);
 	return -1;
     }
 
-    if (src_str && *src_str && dst_str && *dst_str) {
+    if (data && *data && dst_str && *dst_str) {
 
 	/* Make source relative to where we are */
 	char * cwd = HTGetCurrentDirectoryURL();
-	char * full_src_str = HTParse(src_str, cwd, PARSE_ALL);
 
-	fprintf(stdout, "Saving %s to %s\n", full_src_str, dst_str);
+	fprintf(stdout, "Posting to %s\n", dst_str);
 
 	/* Create a request */
 	request = HTRequest_new();
 
-	/* Get an anchor object for the src and dest URIs */
-	src = HTAnchor_findAddress(full_src_str);
+	/* Get an anchor object for the destination URI */
 	dst = HTAnchor_findAddress(dst_str);
 
-	/* PUT the source to the dest */
-	status = HTPutDocumentAnchor(HTAnchor_parent(src), dst, request);
+	/*
+	** Dream up a source anchor (an editor can for example use this).
+	** After creation we associate the data that we want to post and
+	** set some metadata about what the data is. More formats can be found
+	** ../src/HTFormat.html
+	*/
+	src = HTTmpAnchor(NULL);
+	HTAnchor_setDocument(src, data);
+	HTAnchor_setFormat(src, WWW_PLAINTEXT);
+
+	/*
+	** If not posting to an HTTP/1.1 server then content length MUST be
+	** there. If HTTP/1.1 then it doesn't matter as we just use chunked
+	** encoding under the covers
+	*/
+	HTAnchor_setLength(src, strlen(data));
+
+	/* POST the source to the dest */
+	status = HTPostAnchor(src, dst, request);
 
 	/* We don't need these anymore */
 	HT_FREE(cwd);
-	HT_FREE(full_src_str);
 
 	/* Go into the event loop... */
 	if (status == YES) HTEventList_loop(request);
