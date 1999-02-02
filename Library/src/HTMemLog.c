8709e66a5c3811e0fe4cf883ed648b4720225fc4
Henrik Frystyk Nielsen <frytstik@w3.org>
1999-02-02 20:28:11 +0000

    Fixed problem on platforms w/o O_SYNC

--- Library/src/HTMemLog.c
+++ Library/src/HTMemLog.c
@@ -15,11 +15,11 @@
 #include "HTMemLog.h"
 #include "HTTimer.h"
 
-#ifdef WWW_MSWINDOWS
-#define OPEN_FLAGS	O_WRONLY|O_CREAT|O_TRUNC
-#else /* WWW_MSWINDOWS */
+#ifdef O_SYNC
 #define OPEN_FLAGS	O_WRONLY|O_CREAT|O_TRUNC|O_SYNC
-#endif /* !WWW_MSWINDOWS */
+#else
+#define OPEN_FLAGS	O_WRONLY|O_CREAT|O_TRUNC
+#endif
 
 PRIVATE size_t		LogBuffSize = 1024; /* default size is 1k */
 PRIVATE int		LogFd = 2;
