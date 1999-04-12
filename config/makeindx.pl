a19f24887630674732b45a0fcd340bcba978c304
Henrik Frystyk Nielsen <frytstik@w3.org>
1999-04-12 18:56:48 +0000

    Added new modules to document header

--- config/makeindx.pl
+++ config/makeindx.pl
@@ -149,23 +149,27 @@ sorted by interface module. For more comprehensive
 descriptions, see the <A HREF="../User/">user
 documentation</A>
 <UL>
-<LI><A HREF=#WWWApp>Application Utilities and access functions</A>
-<LI><A HREF=#WWWCache>Persistent HTTP/1.1 cache</A>
+<LI><A HREF=#WWWUtil>Basic Utilities</A>
 <LI><A HREF=#WWWCore>Libwww core functions</A>
-<LI><A HREF=#WWWDir>Directory listings etc.</A>
-<LI><A HREF=#WWWFTP>FTP access</A>
+<LI><A HREF=#WWWInit>Initialization Utilities</A>
+<LI><A HREF=#WWWApp>Application Utilities and Access functions</A>
+<LI><A HREF=#WWWTrans>Transport Package</A>
+<LI><A HREF=#WWWMux>Mux Protocol (experimental)</A>
+<LI><A HREF=#WWWMIME>MIME Package</A>
 <LI><A HREF=#WWWFile>Local file access</A>
 <LI><A HREF=#WWWGophe>Gopher access</A>
-<LI><A HREF=#WWWHTML>HTML parser</A>
+<LI><A HREF=#WWWFTP>FTP access</A>
 <LI><A HREF=#WWWHTTP>HTTP access</A>
-<LI><A HREF=#WWWMIME>MIME parser</A>
-<LI><A HREF=#WWWMux>Mux Protocol (experimental)</A>
 <LI><A HREF=#WWWNews>News access</A>
-<LI><A HREF=#WWWSQL>SQL logging facilities</A>
 <LI><A HREF=#WWWTelnt>Telnet access</A>
-<LI><A HREF=#WWWUtil>Basic Utilities</A>
 <LI><A HREF=#WWWWAIS>WAIS access</A>
+<LI><A HREF=#WWWCache>Persistent HTTP/1.1 cache</A>
+<LI><A HREF=#WWWStream>Stream Package</A>
+<LI><A HREF=#WWWDir>Directory Browsing Package</A>
 <LI><A HREF=#WWWZip>Zlib deflate/inflate streams</A>
+<LI><A HREF=#WWWHTML>HTML parser</A>
+<LI><A HREF=#WWWXML>XML parser</A>
+<LI><A HREF=#WWWSQL>SQL logging facilities</A>
 </UL>
 EndOfHeader
 }
