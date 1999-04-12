f119901cc927d3fba8eec2889ea0c5d6c0a753ef
Henrik Frystyk Nielsen <frytstik@w3.org>
1999-04-12 23:59:07 +0000

    updated windows version number

--- Library/src/windows/config.h
+++ Library/src/windows/config.h
@@ -106,7 +106,7 @@
 #define W3C_PACKAGE "w3c-libwww"
 
 /* Define to be the version. */
-#define W3C_VERSION "5.2.9"
+#define W3C_VERSION "5.2.8"
 
 /* Define to be the location of the W3C Icons */
 #define W3C_ICONS "/usr/local/share/w3c-icons"
